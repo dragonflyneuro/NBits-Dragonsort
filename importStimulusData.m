b8ea029b5d2b19a08514be9897fa7b68262b23e7
MICROWAVE\dsdan <dsdanielko@gmail.com>
2020-05-17 19:42:47 +0100

    Can now import stimulus sample database
    import a single vector for event points to be highlighted, a (2,X) vector for event regions to be highlighted

---
+++ importStimulusData.m
@@ -0,0 +1,34 @@
+function [output, statusStr] = importStimulusData(fN)
+
+output = [];
+input = load(fN);
+vars = fieldnames(input);
+
+if length(vars) > 2 || length(vars) < 1
+    statusStr = "Too many or too few variables found!";
+    return;
+end
+
+% 1 variable case
+if length(vars) == 1
+    if min(size(input.(vars{1}))) > 2 || min(size(input.(vars{1}))) < 1
+        statusStr = "Variable size does not make sense!";
+        return;
+    end
+    
+    output = input.(vars{1});
+    
+% 2 variable case
+else
+    if input.(vars{1}) > input.(vars{2})
+        output(1,:) = input.(vars{2});
+        output(2,:) = input.(vars{1});
+    else
+        output(1,:) = input.(vars{1});
+        output(2,:) = input.(vars{2});
+    end
+end
+
+statusStr = "Ready";
+
+end
--- switchButtons.m
+++ switchButtons.m
@@ -53,6 +53,7 @@ switch opt
         app.TagmanagementButton.Enable = 'off';
         app.AutocreatejunkunitsButton.Enable = 'off';
         app.AutocreateunitsButton.Enable = 'off';
+        app.ImportstimulusdataButton.Enable = 'off';
     case 3
         app.AddspikeButton.Enable = 'on';
         app.AddspikeMenu.Enable = 'on';
@@ -99,5 +100,6 @@ switch opt
         app.TagmanagementButton.Enable = 'on';
         app.AutocreatejunkunitsButton.Enable = 'on';
         app.AutocreateunitsButton.Enable = 'on';
+        app.ImportstimulusdataButton.Enable = 'on';
 end
 end
